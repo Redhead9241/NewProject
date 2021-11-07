85b4141f67da92411e00e2f391b144b5bc16e3f1
Dima <ved.dmitriy9241@gmail.com>
2021-11-07 20:23:03 +0200

    GettersSetters

---
+++ Regular/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// new RegExp('pattern', 'flags');
+// /pattern/flags
+
+// const ans = prompt('Введите Ваше число');
+
+// const reg = /\d/g;
+// console.log(ans.match(reg));
+
+const str = 'My name is R2D2';
+
+console.log(str.match(/\W/g));
+// \D
+// \W
+
+
+// \d
+// \w
+// \s
+
+
+
+
+
+
+
+// i
+// g 
+// m
+
+// console.log(ans.search(reg));
+// console.log(ans.match(reg));
+
+// const pass = prompt('Password');
+
+// console.log(pass.replace(/\&/g, "*"));
+
+// console.log('12-34-56'.replace(/-/g, ':'));
+
