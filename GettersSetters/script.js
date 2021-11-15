98a5058421770b4caa122dfae10042b0925e6ee4
Dima <ved.dmitriy9241@gmail.com>
2021-11-15 11:02:44 +0200

    incapsul

--- GettersSetters/script.js
+++ GettersSetters/script.js
@@ -1,18 +1,46 @@
 'use strict';
 
-const persone = {
-    name: 'Alex',
-    age: 25,
+// const persone = {
+//     name: 'Alex',
+//     age: 25,
 
-    get userAge() {
-        return this.age;
-    },
+//     get userAge() {
+//         return this.age;
+//     },
 
-    set userAge(num) {
-        this.age = num;
+//     set userAge(num) {
+//         this.age = num;
+//     }
+// };
+
+// console.log(persone.userAge = 30);
+// console.log(persone.userAge);
+
+class User {
+    constructor (name, age) {
+        this.name = name;
+        this.age = age;
     }
-};
 
-console.log(persone.userAge = 30);
-console.log(persone.userAge);
+    #surname = 'Ved';
 
+
+    
+    say() {
+        return console.log(`Его зовут ${this.name} ${this.#surname} и ему ${this.age}`);
+    }
+
+    get surname() {
+        return this.#surname;
+    }
+
+    set surname(surname) {
+        this.#surname = surname;
+    }
+}
+
+let dima = new User('Dima', 28);
+// dima.sername = 'Ved';
+dima.surname = 'Tesla';
+console.log(dima.say());
+console.log(dima.surname);
